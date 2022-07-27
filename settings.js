c9f85a06a9a51385b14a494fedea2d78ab5d2bdb
sahan2026 <108446606+sahan2026@users.noreply.github.com>
2022-07-27 16:08:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94729359255']
+global.premium = ['94729359255']
+global.ownernomer = '94729359255'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
